0df706c31854939e1b6bede75e6185d2c47253dd
Kale <125678484@qq.com>
2021-10-12 11:31:56 +0800

    添加coroutines

--- mvi-compose-arch/src/main/java/com/kale/simpl/mvi/BaseViewModel.kt
+++ mvi-compose-arch/src/main/java/com/kale/simpl/mvi/BaseViewModel.kt
@@ -1,7 +1,16 @@
 package com.kale.simpl.mvi
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 
 //需要维护事件转换成为viewstate回调，viewmodel对接的是一个个的compose布局，compose为单activity的页面。
-abstract class BaseViewModel<A:IAction,VS:IViewState>:IViewModel<A,VS> , ViewModel()
+abstract class BaseViewModel<A : IAction, VS : IViewState> : IViewModel<A, VS>, ViewModel() {
+
+    protected abstract val _viewState: MutableStateFlow<IViewState>
+
+
+    val viewState = _viewState.asStateFlow()
+
+}
